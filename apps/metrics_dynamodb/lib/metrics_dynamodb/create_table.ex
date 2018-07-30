645a58290cd9f715efc9f79993e95d7f5772975a
Gabriel Klöckner <gabriel.klockner@gmail.com>
2018-07-30 09:29:38 -0300

    Remove deps folder

--- apps/metrics_dynamodb/lib/metrics_dynamodb/create_table.ex
+++ apps/metrics_dynamodb/lib/metrics_dynamodb/create_table.ex
@@ -4,8 +4,8 @@ defmodule MetricsDynamoDB.CreateTable do
 
   alias ExAws.Dynamo
 
-  def call do
-    ExAws.Dynamo.create_table("Users", "email", %{email: :string}, 1, 1)
+  def call(table_name, primary_key) do
+    ExAws.Dynamo.create_table(table_name |> String.capitalize, primary_key, %{primary_key |> String.to_atom => :string}, 1, 1)
     |> ExAws.request!
   end
 end
